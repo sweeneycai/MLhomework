6600c126fdb1945712fee68a8cb282901a401611
sweeneycai <juanxincai0526@gmail.com>
2018-10-31 21:56:20 +0800

    Add change learning rate

---
+++ Lecture6/ANN_change_learning_rate.py
@@ -0,0 +1,57 @@
+import numpy
+from scipy.special import expit
+import matplotlib.pyplot as plt
+
+# NetWork Framework
+class NN:
+
+    # init function
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate, reg, ):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        self.lr = learningrate
+        self.reg = reg
+        
+        # init weight between input layer and hidden layer, hidden layer and output layer, the weights obbey normal distribution.
+        self.weight_ih = numpy.random.normal(0.0, pow(self.hnodes, 0.5), (self.hnodes, self.inodes))
+        self.weight_ho = numpy.random.normal(0.0, pow(self.onodes, 0.5), (self.onodes, self.hnodes))
+        
+        # activation function
+        self.activation = lambda x: expit(x)
+
+    # NetWork training
+    def train(self, input_list, target_list, learning_rate, epoch):
+
+        self.lr = learning_rate * pow(0.9, (epoch / 50))
+        # init inputs and target
+        inputs = numpy.array(input_list, ndmin=2).T
+        target = numpy.array(target_list, ndmin=1)
+
+        # calculate hidden layer inputs and hidden layer outputs
+        hidden_inputs = numpy.dot(self.weight_ih, inputs)
+        hidden_outputs = self.activation(hidden_inputs)
+
+        # calculate final layer inputs and outputs
+        final_inputs = numpy.dot(self.weight_ho, hidden_outputs)
+        final_outputs = self.activation(final_inputs)
+
+        
+        # calculate final layer errors and hidden layer errors, use BackPropagation algorithm.
+        output_errors = target - final_outputs
+        hidden_errors = numpy.dot(self.weight_ho.T, output_errors)
+        
+        # update weight
+        self.weight_ho += self.lr * (numpy.dot(output_errors * final_outputs * (1 - final_outputs), hidden_outputs.T) + self.reg * numpy.sum(numpy.sign(self.weight_ho))) 
+        self.weight_ih += self.lr * (numpy.dot(hidden_errors * hidden_outputs * (1 - hidden_outputs), inputs.T) + self.reg * numpy.sum(numpy.sign(self.weight_ih)))
+
+        # self.lr = pow(0.95, epoch) * learning_rate
+    
+    # Query NN
+    def query(self, input_list):
+        inputs = numpy.array(input_list, ndmin=2).T
+        hidden_inputs = numpy.dot(self.weight_ih, inputs)
+        hidden_outputs = self.activation(hidden_inputs)
+        final_inputs = numpy.dot(self.weight_ho, hidden_outputs)
+        final_outputs = self.activation(final_inputs)
+        return final_outputs
--- Lecture6/decrease_learning_rate.py
+++ Lecture6/decrease_learning_rate.py
@@ -1,26 +1,31 @@
-from ANN import NN
+from ANN_change_learning_rate import NN
 from shuffle import Data
 import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
     training_x, training_y, test_x, test_y = Data.getData()
-    learning_rate = 0.1
+    learning_rate = 1
     reg = 0
     input_nodes = 2
     hidden_nodes = 5
     output_nodes = 1
-    epoch = 5000  
+    epoch = 2000  
     loss_train_all = []
     loss_test_all = []
 
     plt.ion()
+    plt.subplot(121)
     plt.grid(True)
-    # plt.ylim(0, 10)
     plt.xlim(0, epoch)
     plt.xlabel("Generation")
     plt.ylabel("Loss")
     plt.title("The relationship between generations and loss")
+    plt.subplot(122)
+    plt.grid(True)
+    plt.xlim(0, epoch)
+    plt.xlabel("Generation")
+    plt.ylabel("Learning rate")
 
     n = NN(input_nodes, hidden_nodes, output_nodes, learning_rate, reg)
 
@@ -29,7 +34,8 @@ def main():
         for[j, x] in enumerate(training_x):
             inputs = np.array(x) / 100
             targets = training_y[j]
-            n.train(inputs, targets)
+            n.train(inputs, targets, learning_rate, i)
+        print(n.lr)
 
         # calculate training set loss and accuracy
         loss_train = 0
@@ -38,7 +44,6 @@ def main():
             result = n.query(np.array(x) / 100)
             loss_train += pow((result - training_y[j]), 2) / 2
             training_result.append(result)
-
                     
         # calculate test set loss and accuracy
         loss_test = 0
@@ -51,17 +56,14 @@ def main():
         loss_train_all.append(float(loss_train / 8))
         loss_test_all.append(float(loss_test / 2))
 
-        # decrease learning rate
-        learning_rate /= 1.1
-
-        if i % 50 == 0:
+        if i % 10 == 0:
+            plt.subplot(121)
             plt.scatter(i, (loss_train / 8), marker="^", c='b')
             plt.scatter(i, (loss_test / 2), marker='o', c='r')
+            plt.subplot(122)
+            plt.scatter(i, n.lr, marker='o', c='r')
             plt.pause(0.1)
-
-            print(i)
-    
-        plt.savefig('change_learning_rate.png')
+            plt.savefig('change_learning_rate.png')
 
 
 if __name__ == "__main__":
